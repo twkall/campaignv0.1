fb0b4427c1d74d8bf43e95c57a1eb0298c1e37bf
Muhamad Iqbal Tawakal <mcbalbal@live.com>
2018-04-15 15:23:02 +0700

    build 2 v0.1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    List,
+    Menu,
+    Responsive,
+    Segment,
+    Sidebar,
+    Visibility,
+  } from 'semantic-ui-react';
+  import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
